7f541b0e9cdbe7e9828317f1f414458349d31db7
Yogesh_code_pirates <chaudhariy097@gmail.com>
2024-03-20 11:31:26 +0530

    changes in project mangment

---
+++ quitz app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Quiz from './components/Quiz'
+import Header from './components/Header'
+import { Container } from '@chakra-ui/react'
+function App() {
+  return (
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
+
+export default App
---
+++ quitz app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+const Header = () => {
+  return (
+    <Heading as={'h1'} size={'lg'}>Quiz App</Heading>
+  )
+}
+
+export default Header
